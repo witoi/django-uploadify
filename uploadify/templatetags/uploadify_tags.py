158abeef11e604c51359fff56a6cab8a9002f23d
Pedro Burón <pedro@witoi.com>
2012-04-12 16:15:59 -0300

    splitted scripts from template added inline param to templatetag

--- uploadify/templatetags/uploadify_tags.py
+++ uploadify/templatetags/uploadify_tags.py
@@ -7,7 +7,7 @@ register = template.Library()
 #   multi_file_upload
 # -----------------------------------------------------------------------------
 @register.inclusion_tag('uploadify/multi_file_upload.html', takes_context=True)
-def multi_file_upload(context, upload_complete_url, extra=None, callback=None):
+def multi_file_upload(context, upload_complete_url, extra=None, callback=None, inline=True):
     """
     Displays a Flash-based interface for uploading multiple files.
     When all files have been uploaded, the given URL is POSTed to.  The returned
@@ -18,10 +18,16 @@ def multi_file_upload(context, upload_complete_url, extra=None, callback=None):
     * allBytesLoaded - The total number of bytes uploaded
     * speed - The average speed of all uploaded files
     """
-    return { 
+    template_context = { 
         'callback': callback,
         'upload_complete_url' : upload_complete_url,
         'uploadify_path' : settings.UPLOADIFY_PATH,
         'upload_path' : settings.UPLOADIFY_UPLOAD_PATH,
         'extra': extra,
+        'inline': inline,
     }
+
+    if not inline:
+        context.update({'multi_file_upload_scripts': render_to_string('uploadify/scripts.html', template_context)})
+
+    return template_context
